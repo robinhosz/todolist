21d90914e530b572aeb4121a6d283c82e7584c25
José Robson <joserobsonsiqueira23@hotmail.com>
2022-08-30 23:43:24 -0300

    feat: delete itens

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -17,4 +17,16 @@ export class TodoListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm("Você deseja realmente deletar tudo?");
+    if(confirm) {
+      this.taskList = [];
+    }
+   
+  }
+
 }
